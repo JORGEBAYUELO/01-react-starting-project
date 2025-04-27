b47e576906b64f4c1af132326af41014ab5f88d1
Jorge Bayuelo <jorge3design@gmail.com>
2025-04-27 17:17:53 -0500

    add function for tab buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,9 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  let [selectedTopic, setSelectedTopic] = useState("Please click a button");
-
   function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic = selectedButton;
-    console.log(selectedTopic);
+    // SelectedButton => "components", "jsx", "props", "state"
+    console.log(selectedButton);
   }
   return (
     <div>
@@ -33,14 +30,13 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("componetns")}>
+            <TabButton onSelect={() => handleSelect("components")}>
               Components
             </TabButton>
             <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
         </section>
       </main>
     </div>
